e4c1474f410e73925d87a3eb857fe0274771e14f
Victor Carvalho <victor.blq@gmail.com>
2019-05-28 22:30:19 -0300

    first commit, pristine server and client

---
+++ server/src/index.js
@@ -0,0 +1,16 @@
+import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+
+const app = express();
+
+//CORS
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send("Working");
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`App listening on port ${process.env.PORT}`);
+});
